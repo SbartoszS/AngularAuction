dfd10851fc56391dfc29dc78bc997f889b305b80
BStojek <es.bartek@gmail.com>
2020-07-27 19:01:43 +0200

    Update product.module.ts

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { RouterModule } from '@angular/router';
 import { ProductComponent } from './product.component';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
-import { ProductSuggestionComponent } from './product-suggestion/product-suggestion.component';
-
-
 
 @NgModule({
-  declarations: [ProductComponent, ProductDetailComponent, ProductSuggestionComponent],
+  declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    FlexLayoutModule,
+    RouterModule.forChild([{ path: '', component: ProductComponent }]),
+  ],
 })
-export class ProductModule { }
+export class ProductModule {}
