167920e6ef1ca8384d525a11a88e08b434733c5b
BStojek <es.bartek@gmail.com>
2020-07-27 18:54:22 +0200

    product components
    generate 3 new components for product

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ProductComponent } from './product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductSuggestionComponent } from './product-suggestion/product-suggestion.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ProductComponent, ProductDetailComponent, ProductSuggestionComponent],
   imports: [
     CommonModule
   ]
