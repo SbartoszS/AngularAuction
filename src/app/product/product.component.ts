78c38369d8a9e78eac2089763c4d9986befcb2eb
BStojek <es.bartek@gmail.com>
2020-07-27 19:26:23 +0200

    Update product.component.ts

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService, Product } from '../shared/services';
+import { Observable } from 'rxjs';
+import { map, filter, switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'nga-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
-export class ProductComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class ProductComponent {
+  product$: Observable<Product>;
+  suggestedProducts$: Observable<Product[]>;
+  constructor(
+    private route: ActivatedRoute,
+    private productService: ProductService
+  ) {
+    this.product$ = this.route.paramMap.pipe(
+      map((params) => parseInt(params.get('productId') || '', 10)),
+      filter((productId) => !!productId),
+      switchMap((productId) => this.productService.getById(productId))
+    );
+    this.suggestedProducts$ = this.productService.getAll();
   }
-
 }
