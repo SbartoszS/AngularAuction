1ed8f60d2b570d86b9b06d803673288294dda2b2
BStojek <es.bartek@gmail.com>
2020-07-27 18:58:00 +0200

    Update app.routing.ts

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -5,4 +5,9 @@ export const routes: Route[] = [
     path: '',
     loadChildren: () => import('./home/home.module').then((m) => m.HomeModule),
   },
+  {
+    path: 'products/:productId',
+    loadChildren: () =>
+      import('./product/product.module').then((m) => m.ProductModule),
+  },
 ];
