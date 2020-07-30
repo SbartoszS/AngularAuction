f5617507031e24645b8cf00afef324191f5044ad
BStojek <es.bartek@gmail.com>
2020-07-30 11:50:41 +0200

    angular forms, refactor code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,37 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { FlexLayoutModule } from '@angular/flex-layout';
-import { HttpClientModule } from '@angular/common/http';
-import { RouterModule } from '@angular/router';
 
+import { SHARED_SERVICES } from './shared/services';
+import { SearchFormModule } from './shared/components';
 import { AppComponent } from './app.component';
-import { ProductService, Product } from './shared/services';
 import { routes } from './app.routing';
 
 @NgModule({
-  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    FlexLayoutModule,
+    RouterModule.forRoot(routes),
+
     MatButtonModule,
     MatIconModule,
+    MatSidenavModule,
     MatToolbarModule,
-    FlexLayoutModule,
-    HttpClientModule,
-    RouterModule.forRoot(routes),
+
+    SearchFormModule,
   ],
-  providers: [ProductService],
+  declarations: [AppComponent],
   bootstrap: [AppComponent],
+  providers: [...SHARED_SERVICES],
 })
 export class AppModule {}
