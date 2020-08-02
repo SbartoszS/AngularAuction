d47e478383398f32e2388e21e0d58bf327904d46
BStojek <es.bartek@gmail.com>
2020-08-02 14:44:21 +0200

    add e2e test for search products

---
+++ client/e2e/src/search.e2e-spec.ts
@@ -0,0 +1,24 @@
+import { SearchPage } from './search.po';
+import { browser } from 'protractor';
+
+describe('Wyszukiwanie ngAuction', () => {
+  let searchPage: SearchPage;
+
+  beforeEach(() => {
+    searchPage = new SearchPage();
+  });
+
+  it('powinno przeprowadzać wyszukiwanie produków z przedziału cenowego od 10 do 100 dolarów', async () => {
+    searchPage.navigateToLandingPage();
+    let url = await browser.getCurrentUrl();
+    expect(url).toContain('/categories/wszystkie%produkty');
+
+    searchPage.performSearch(10, 100);
+    url = await browser.getCurrentUrl();
+    expect(url).toContain('/search?minPrice=10&maxPrice=100');
+
+    const firstProductPrice = await searchPage.getFirstProductPrice();
+    expect(firstProductPrice).toBeGreaterThan(10);
+    expect(firstProductPrice).toBeLessThan(100);
+  });
+});
---
+++ client/e2e/src/search.po.ts
@@ -0,0 +1,49 @@
+import { protractor, browser, by, element, $, $$ } from 'protractor';
+
+export class SearchPage {
+  performSearch(minimalPrice: number, maximumPrice: number) {
+    const searchOnToolbar = element(by.id('search'));
+    searchOnToolbar.click();
+
+    const minPrice = $('input[formControlName="minPrice"]');
+    const maxPrice = $('input[formControlName="maxPrice"]');
+
+    const searchOnForm = element(by.buttonText('SZUKAJ'));
+
+    searchOnForm.click();
+
+    const EC = protractor.ExpectedConditions;
+    const urlChange = EC.urlContains('/search');
+    browser.wait(urlChange, 5000, 'Adres URL powinien zawierać /search');
+  }
+
+  navigateToLandingPage() {
+    return browser.get('/');
+  }
+
+  getFirstProductPrice() {
+    return $$('span[class="tile__price-tag"]')
+      .first()
+      .getText()
+      .then((value) => {
+        return parseInt(value.replace('$', ''), 10);
+      });
+  }
+
+  waitForUrlTocontain(
+    urlSegment: string,
+    timeout: number,
+    timeoutMessage: string
+  ) {
+    return browser.wait(
+      () => {
+        return browser.getCurrentUrl().then((url) => {
+          const regex = new RegExp(urlSegment);
+          return regex.test(url);
+        });
+      },
+      timeout,
+      timeoutMessage
+    );
+  }
+}
